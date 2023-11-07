7c7ab7ccf2f32e96e7232d8de8c2aaf02e26ba8b
aheath <aaron.heath5447@gmail.com>
2023-11-07 08:12:09 -0500

    Add forecast. Add styling for forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 // Set Globals
 const geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city-name}&limit=1&appid="
 const weatherUrl =  'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid='
+const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=imperial&appid='
 const exclusions = "&exclude=minutely,hourly,alerts"
-const dateFormat = "MMMM D, YYYY" 
+const todayFormat = "MMMM D, YYYY"
+const forecastFormat = "ddd M/D"
 // This would normally be obscured as an environment variable
 const key = "b86517d6f9f09daf180408ee9981a4cc"
 
@@ -15,6 +17,7 @@ $(function () {
     // Select Main Elements
     var $mainContent = $("main").children("div");
     var $activeContainer = $("#active-search");
+    var $forecastContainer = $('#forecast');
     var $previousContainer = $("#previous-search");
 
     var $cityInput = $("#city-input")
@@ -52,7 +55,7 @@ $(function () {
     function weatherFetch(cityData) {
         // Build fetch url
         console.log(cityData.lon);
-        var url = weatherUrl.replace("{lat}",cityData.lat).replace("{lon}",cityData.lon) + key + exclusions;
+        var url = forecastUrl.replace("{lat}",cityData.lat).replace("{lon}",cityData.lon) + key + exclusions;
 
         // Fetch Data
         fetch(url)
@@ -69,10 +72,11 @@ $(function () {
     function renderWeather(cityData,weatherData) {
         // Clear Active Container
         $activeContainer.html("");
-
+        $forecastContainer.html("")
+        
         // Assign needed data for easier reference
-        var today = dayjs().format(dateFormat);
-        var mainWeather = weatherData.main;
+        var today = dayjs().format(todayFormat);
+        var mainWeather = weatherData.list[0].main;
 
         // Create and append city header
         var $cityName = $("<h2>");
@@ -87,28 +91,85 @@ $(function () {
 
         // Create and append weather icon
         var $icon = $("<img>")
-        var iconSource = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png"
+        var iconSource = "http://openweathermap.org/img/w/" + weatherData.list[0].weather[0].icon + ".png"
         $icon.attr('src',iconSource);
         $icon.attr('alt', "Weather Icon");
         $activeContainer.append($icon);
 
         // Create and append Temp
         var $temp = $('<p>')
-        $temp.text(`Temp: ${weatherData.main.temp} °F`)
+        $temp.text(`Temp: ${Math.round(weatherData.list[0].main.temp)} °F`)
         $activeContainer.append($temp);
 
         // Create and append Wind
         let $wind = $("<p>")
-        $wind.text(`Wind: ${weatherData.wind.speed} MPH`);
+        $wind.text(`Wind: ${weatherData.list[0].wind.speed} MPH`);
         $wind.addClass("weather-stat");
         $activeContainer.append($wind);
         
         // Create and append Humidity
         let $humidity = $('<p>');
-        $humidity.text(`Humidity: $${weatherData.main.humidity}%`);
+        $humidity.text(`Humidity: ${weatherData.list[0].main.humidity}%`);
         $activeContainer.append($humidity);
 
+        // Loop for forecast cards
+        for(let i = 8; i < weatherData.list.length; i+=8) {
+            
+            // Get this iteration's weather data for ease of access
+            let iterWeather = weatherData.list[i];
+            let iterDate = dayjs(iterWeather.dt_txt).format(forecastFormat);
+            // Create parent element
+            let $forecastCard = $("<div>");
+            $forecastCard.addClass("forecast-card");
 
+            // Date Content
+            let $dateBlock = $("<div>");
+            $dateBlock.addClass("forecast-date");
+            $dateBlock.append($("<p>").addClass("forecast-day").text(iterDate.split(" ")[0]));
+            $dateBlock.append($("<p>").addClass("forecast-monthday").text(iterDate.split(" ")[1]));
+
+            $forecastCard.append($dateBlock);
+
+            // Forecast Content
+            let $forecastContent = $("<div>");
+            // Icon
+            let $iterIcon = $("<img>");
+            let iterIconSource = "http://openweathermap.org/img/w/" + iterWeather.weather[0].icon + ".png"
+            $iterIcon.attr("src",iterIconSource);
+            $iterIcon.attr("alt","Forecast Icon");
+            $forecastCard.append($iterIcon);
+
+            let $paragraph = $("<p>");
+                // Temperature
+            let $iterTemp = $("<span>");
+            $iterTemp.addClass("forecast-temp");
+            $iterTemp.text(`${Math.round(iterWeather.main.temp)}°`);
+            $iterTemp.appendTo($paragraph);
+            $forecastCard.append($paragraph);
+
+            // Humidity & Windspeed
+            let $suppDiv = $("<div>");
+            $suppDiv.attr("class", "supp-weather");
+            $suppDiv.append(
+                $("<p>").text(`Humidity: ${iterWeather.main.humidity}%`)
+                );
+            $suppDiv.append(
+                $("<p>").text(`Wind: ${Math.round(iterWeather.wind.speed)} MPH`)
+                );
+            
+            $forecastCard.append($suppDiv);
+
+
+
+            $forecastContainer.append($forecastCard)
+
+
+
+
+        }
     }
+
+
+
 
 });
